792429f79d017294f7199ca85d1175bfa1cef587
Matt Harris <m.harris@gns.cri.nz>
2026-06-05 14:41:51 +1200

    minor update to script 2

--- HYSPLIT-Script-2-ParseData.R
+++ HYSPLIT-Script-2-ParseData.R
@@ -6,3 +6,10 @@
 # Last modified 05/06/2026
 
 ##### SETUP #####
+
+## Load and/or install packages. 
+
+## CORE PACKAGES: SYNTAX
+pacman::p_load(dplyr,magrittr,lubridate,tibble,R.utils,tidyverse,openair,chron, here)
+
+## TrajSpatial package, for a bunch of Matt's HYSPLIT related functions. Some will overwrite the hys
